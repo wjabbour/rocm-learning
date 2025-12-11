5d7e48e41dea7580bb50336c1cd3e387983e3663
Turner Jabbour <doubleujabbour@gmail.com>
2025-12-10 22:05:59 -0700

    write softmax kernel

---
+++ src/kernels/reduction/v3_softmax/kernel.hip.cpp
@@ -0,0 +1,158 @@
+/*
+    Softmax is one of the most critical kernels for inference. It takes in a list of K real numbers
+    and calculates a probability distribution of K possible outcomes. In inference, this is used
+    to predict the most likely token that should come next in the sequence.
+
+    This is a dummy version of softmax because
+
+    1) the input is an array of floats
+    2) we are only performing these calculations over a single block
+
+    This kernel performs the following steps
+
+    1) First we need to find the maximum value from the input. We do this by each wavefront using warp shuffling
+    to find the maximum value in the wavefront. Once that value is found, each wavefront writes to LDS.
+    2) Now that each wavefront has written its own maximum value, we use warp shuffling again - this time
+    using the first 8 threads (we use 8 because 256 (input size) divided by 32 (RDNA wavefront size) = 8) 
+    in the first wave to find the maximum amongst all 8 values in LDS.
+    3) Now that we have the maximum value from the input, we perform a very similar set of steps to find
+    the sum of the input.
+    4) Once we have the sum of the input and the max of the input we can compute our probability distribution. Each thread
+    picks a value from the input, subtracts the max from the value and computes e^(val - max). Once this computation is complete
+    it yields y. Each thread then writes y / globalSum to the output. This is our probability distribution.
+*/
+
+#include <hip/hip_runtime.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+__device__ float waveReduceSum(float val) {
+    val += __shfl_down(val, 16, 32);
+    val += __shfl_down(val, 8, 32);
+    val += __shfl_down(val, 4, 32);
+    val += __shfl_down(val, 2, 32);
+    val += __shfl_down(val, 1, 32);
+
+    return val;
+}
+
+__device__ float waveReduceMax(float val) {
+    float neighbor = __shfl_down(val, 16);
+    val = max(neighbor, val);
+
+    neighbor = __shfl_down(val, 8);
+    val = max(neighbor, val);
+
+    neighbor = __shfl_down(val, 4);
+    val = max(neighbor, val);
+
+    neighbor = __shfl_down(val, 2);
+    val = max(neighbor, val);
+
+    neighbor = __shfl_down(val, 1);
+    val = max(neighbor, val);
+
+    return val;
+}
+
+__global__ void softmax_kernel(float* input, float* output) {
+    __shared__ float shared_sums[8];
+    __shared__ float shared_max[8];
+
+    int tid = threadIdx.x;
+    int laneId = threadIdx.x % 32;
+    int wfId = threadIdx.x / 32;
+    
+    float val = input[tid];
+
+    // calculate the max for the wavefront using shuffling
+    float max = waveReduceMax(val);
+
+    // one thread from each wavefront is resonsible for writing to LDS
+    if (laneId == 0) {
+        shared_max[wfId] = max;
+    }
+
+    __syncthreads();
+
+    float blockMax = 0.0f;
+
+    // now that all wavefronts have written their max, find the max amongst the wavefronts
+    if (wfId == 0) {
+        blockMax = (laneId < 8) ? shared_max[laneId] : -__FLT_MAX__;
+
+        blockMax = waveReduceMax(blockMax);
+
+        if (laneId == 0) {
+            shared_max[0] = blockMax;
+        }
+    }
+
+    __syncthreads();
+
+    float globalMax = shared_max[0];
+
+    // the max is needed for the expf calculation
+    float expf_val = expf(val - globalMax);
+
+    // calculate the sum for the wavefront using shuffling
+    float sum = waveReduceSum(expf_val); 
+    
+    // one thread from each wavefront is resonsible for writing to LDS
+    if (laneId == 0) {
+        shared_sums[wfId] = sum;
+    }
+    
+    __syncthreads();
+
+    float blockSum = 0.0f;
+
+    // now that all wavefronts have written their sum, find the sum of sums
+    if (wfId == 0) {
+        /*
+            ensure all threads in the wavefront participate. in general, we don't
+            want to allow threads to retrieve data from non-executing threads
+            
+            and adding 0 to the blockSum doesnt change our answer
+        */
+        blockSum = (laneId < 8) ? shared_sums[laneId] : 0.0f;
+
+        blockSum = waveReduceSum(blockSum);
+
+        if (laneId == 0) {
+            shared_sums[0] = blockSum;
+        }
+    }
+
+    __syncthreads();
+
+    float globalSum = shared_sums[0];
+
+    // the answer for each thread
+    output[tid] = expf_val / globalSum;
+}
+
+int main() {
+    int N = 256;
+    size_t bytes = N * sizeof(float);
+
+    std::vector<float> h_in(N, 1.0f); // Input: all 1s
+    std::vector<float> h_out(N);
+
+    float *d_in, *d_out;
+    hipMalloc(&d_in, bytes);
+    hipMalloc(&d_out, bytes);
+
+    hipMemcpy(d_in, h_in.data(), bytes, hipMemcpyHostToDevice);
+
+    hipLaunchKernelGGL(softmax_kernel, dim3(1), dim3(256), 0, 0, d_in, d_out);
+
+    hipMemcpy(h_out.data(), d_out, bytes, hipMemcpyDeviceToHost);
+
+    std::cout << "result: " << h_out[0] << std::endl;
+
+    hipFree(d_in);
+    hipFree(d_out);
+    return 0;
+}
