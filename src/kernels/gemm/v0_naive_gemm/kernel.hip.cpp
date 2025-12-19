6f06836b012480c02450b58bdf37768a45083e6a
Turner Jabbour <doubleujabbour@gmail.com>
2025-12-18 22:19:32 -0700

    add thorough comments throughout GEMM kernel

--- src/kernels/gemm/v0_naive_gemm/kernel.hip.cpp
+++ src/kernels/gemm/v0_naive_gemm/kernel.hip.cpp
@@ -4,32 +4,55 @@
 #define TILE_SIZE 16
 
 __global__ void matmul(float* A, float* B, float* C, int width) {
+    // a TILE_SIZE by TILE_SIZE LDS tile we will use to store the contents of input matrix A that our block will operate on
     __shared__ float tile_A[TILE_SIZE][TILE_SIZE];
+    // a TILE_SIZE by TILE_SIZE LDS tile we will use to store the contents of input matrix B that our block will operate on
     __shared__ float tile_B[TILE_SIZE][TILE_SIZE];
 
+    // coordinates of the thread within the block
     int tx = threadIdx.x;
     int ty = threadIdx.y;
 
+    // coordinates of the thread within the grid
     int col = blockIdx.x * blockDim.x + tx;
     int row = blockIdx.y * blockDim.y + ty;
 
     float sum = 0.0f;
-    // i is 0 through N - 1
+    /*
+        each block of threads walks horizontally along input matrix A and vertically along input matrix B
+
+         input matrices A and B are given as 1D contiguous memory blocks stored in row-major order
+    */
     for (int k = 0; k < width; k += TILE_SIZE) {
+        // all threads in the wavefront request contiguous memory addresses from input matrix A
         int aIdx = (row * width) + tx + k;
+        /*
+            since the input is stored in row major order, we cannot fetch a column of data
+            from B without executing N memory transactions (where N = size of column) per wavefront. To improve
+            memory coalescense, threads in the wavefront collaboratively load in a row of length TILE_SIZE from B, all
+            wavefronts in the block working together to load the full tile from B across TILE_SIZE memory transactions.
+        */
         int bIdx = (k + ty) * width + col;
 
+        // now that we've done the mental gymnastics to identify which piece of data to pull from A and B...
         tile_A[ty][tx] = A[aIdx];
         tile_B[ty][tx] = B[bIdx];
 
+        // before any wavefront can work with the data in LDS, we must ensure that all wavefronts have finished writing to LDS
         __syncthreads();
 
+        /*
+            although the threads of the block work together to collaboratively load LDS, once it's time to calculate, each thread
+            simply needs to walk horizontally along A and vertically along B to perform the matrix multiplication.
 
-        // j is 0 through 15
+            Notice that we are now able to walk along the column in our B tile. We don't have to worry about coalescing our memory
+            reads any longer. Access to LDS is extremely fast for each thread.
+        */
         for (int j = 0; j < TILE_SIZE; j++) {
            sum += tile_A[ty][j] * tile_B[j][tx];
         }
 
+        // ensure that wavefronts in our block do not begin altering LDS data until all of the wavefronts have completed their calculations
         __syncthreads();
     }
 
