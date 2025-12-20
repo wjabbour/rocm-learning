562014ddd591722cf4a67941bdf638c61fb1fb5b
Turner Jabbour <doubleujabbour@gmail.com>
2025-12-19 17:54:35 -0700

    complete summary of naive GEMM kernel

--- src/kernels/gemm/v0_naive_gemm/kernel.hip.cpp
+++ src/kernels/gemm/v0_naive_gemm/kernel.hip.cpp
@@ -4,6 +4,45 @@
 
 #define TILE_SIZE 16
 
+/*
+    This kernel multiplies two matrices and stores the output in a third matrix.
+
+    In order to calculate the value of Cᵢⱼ we need row i from matrix A and column j from matrix B. As the dimensions
+    of the input matrices increase, more and more data is necessary to calculate each element of C.
+
+    Clearly, the kernel is going to need to make many requests from global memory. Therein lies a potential pitfall: although 
+    we think of A and B as matrices, they are stored as 1D contiguous chunks of memory in a format called "row major".
+
+    Row major means that the contents of each row are stored contiguously and adjacent rows are stored contiguously.
+
+    e.g.
+
+    rowMajorArray = [1, 2, 3, 4, 5, 6]
+
+    If we declare that this represents a 3x2 matrix, then that matrix looks like this:
+
+    1 2
+    3 4
+    5 6
+
+    Here you can see that as you traverse down a column, you are actually traversing N steps at a time (Here N is 2, 1 appears
+    at index 0 and 3 appears at index 2). As N grows, the steps you take as you traverse the columns grows.
+
+    Tying this back to the hardware: when the threads of a wavefront request the contents of memory addresses from the Vector Memory Unit
+    it attempts to coalesce these requests into as few requests as possible. It does this by using its knowledge of the platform's
+    cache line size, and determining which memory addresses fit into that size.
+
+    If we were to tell a thread to load all of the data from a column of B, if N * lds_data_type_bytes > cache_line_size then every single
+    data load would need a separate request to global memory. Our kernel would immediately become memory-bound.
+
+    We solve this by breaking the problem into "tiles". We fetch TILE_SIZExTILE_SIZE tiles of data from A and B. This solves two problems with 
+    our data access:
+
+    1) instead of each thread creating an uncoalesced memory transaction, the threads work together to bring wide chunks of data back from global memory
+    which optimizes our use of the available memory bandwidth.
+    2) we reuse the data TILE_SIZE times once loaded into LDS which amortizes the cost of loading from global memory.
+*/
+
 __global__ void matrix_multiply(float* A, float* B, float* C, int M, int N, int K) {
     // a TILE_SIZE by TILE_SIZE LDS tile we will use to store the contents of input matrix A that our block will operate on
     __shared__ float tile_A[TILE_SIZE][TILE_SIZE];
@@ -94,8 +133,8 @@ int main() {
         is generalizable to matrices of arbitrary dimensions
     */
     int M = 2;
-    int N = 2;
-    int K = 1;
+    int N = 3;
+    int K = 2;
 
     // given by the definition of matrix multiplication
     const size_t bytesA = M * K * sizeof(float);
@@ -103,8 +142,8 @@ int main() {
     const size_t bytesC = M * N * sizeof(float);
 
     // allocate host memory
-    std::vector<float> A_h = {1.0f, 2.0f};
-    std::vector<float> B_h = {1.0f, 2.0f};
+    std::vector<float> A_h = {1.0f, 2.0f, 2.0f, 4.0f};
+    std::vector<float> B_h = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f};
     std::vector<float> C_h(M * N, 0.0f);
  
     // allocate device memory
